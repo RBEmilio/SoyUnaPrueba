2f230c9cff8c69e6987eaacf21ec71cfc23376fc
RBEmilio <emiliosepr@gmail.com>
2024-11-04 17:29:20 +0100

    Comentarios

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -5,31 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
+        //Inicializamos el PlayerPref "char" en 0
         PlayerPrefs.SetInt("Char", 0);
-        PlayerPrefs.Save();
+        PlayerPrefs.Save(); //Guardas las playerPrefs
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-
+    //Método del botón rojo
     public void RedOne()
     {
+        //Colocamos en "char" el valor 1
         PlayerPrefs.SetInt("Char", 1);
-        PlayerPrefs.Save();
-        SceneManager.LoadScene("Game");
+        PlayerPrefs.Save(); //Guardamos las especificaciones
+        SceneManager.LoadScene("Game"); //Cambio de escena
     }
-
+    //Método del botón verde
     public void GreenOne()
     {
+        //Colocamos en "char" el valor 2
         PlayerPrefs.SetInt("Char", 2);
-        PlayerPrefs.Save();
-        SceneManager.LoadScene("Game");
+        PlayerPrefs.Save();//Guardamos las especificaciones
+        SceneManager.LoadScene("Game");//Cambio de escena
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Inicializamos una variable del tipo "render", para manegar el aspecto
     private Renderer render;
 
+    //Variable donde almacenar el playerPref
     public int charNum;
 
+    //Dos variables para pasarle por el inspector los dos materiales
     public Material matrRed;
     public Material matrGreen;
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        //Obtengo el componente render del game object asociado al script
         render = GetComponent<Renderer>();
+
+        //Guardamos el playerPref en la varible charNum
         charNum = PlayerPrefs.GetInt("Char");
 
     }
@@ -22,24 +28,27 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //llamada al método Establecer color, tambien se puede llamar en el start
         EstablecerColor();
     }
 
+    //método para establecer el color. Puede ser privado.
     public void EstablecerColor()
     {
+        //Lee el charNum y busca a que caso corresponde.
         switch (charNum)
         {
             case 0:
-                Debug.Log("Error");
+                Debug.Log("Error"); //Si charNum = 0, ocurre esto.
                 break;
             case 1:
-                render.material = matrRed;
+                render.material = matrRed; //Si charNum = 1, coloca el material rojo.
                 break;
             case 2:
-                render.material = matrGreen;
+                render.material = matrGreen; //Si charNum = 2, coloca el material verde.
                 break;
             default:
-                Debug.Log("Error, no encontrado");
+                Debug.Log("Error, no encontrado"); //Si charNum = 1, ocurra esto.
                 break;
         }
     }
